a89997e618af2ece6cacc9ec1cba82690c4bbee4
AlexeyRusak <alexey_rusak@mail.ru>
2023-03-18 15:20:41 +0200

    Ready tasks for senf to GitHub

---
+++ task1_PS/task1.ps1
@@ -0,0 +1,75 @@
+#Add parameters
+param(
+    [Parameter()]
+    [string]$ip_address_1,
+
+    [Parameter()]
+    [string]$ip_address_2,
+
+    [Parameter()]
+    [string]$network_mask
+)
+
+#Check of 3 arguments.
+if ($PSBoundParameters.Count -ne 3){
+    Write-Error -Message "It should be 3 arguments. Please try again" -ErrorAction Stop 
+}
+
+#Pattern of IP address from 0.0.0.0 - 255.255.255.255
+$pattern = "^([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
+
+#Pattern of Subnet prefix 0-32
+$pattern2 = "^([0-9]|[12][0-9]|3[012])$"
+
+
+#Check of input format
+if (!($ip_address_1 -match $pattern)){
+    Write-Error -Message "First argument should be IP address. Format should be x.x.x.x .  Please try again." -ErrorAction Stop  
+}
+
+elseif (!($ip_address_2 -match $pattern)) {
+    Write-Error -Message "Second argument should be IP address. Format should be x.x.x.x .  Please try again." -ErrorAction Stop 
+}
+
+elseif ((!($network_mask -match $pattern)) -and (!($network_mask -match $pattern2)))  {
+    Write-Error -Message "Third argument should be Subnet mask address or subnet prefix. Format should be x.x.x.x or for prefix xx .  Please try again." -ErrorAction Stop  
+}
+
+
+else {
+
+    #If user type prefix doing some calculation to convert prefix to int
+    if ($network_mask -match $pattern2){
+
+        $bitString=('1' * $network_mask).PadRight(32,'0')
+
+        $ipString=[String]::Empty
+
+        #Make 1 string combining a string for each byte and convert to int
+        for($i=0;$i -lt 32;$i+=8){
+
+            $byteString=$bitString.Substring($i,8)
+            $ipString+="$([Convert]::ToInt32($byteString, 2))."
+
+        }
+            $mask = [ipaddress]$ipString.TrimEnd('.')
+    
+    }
+    else {
+
+        $mask = [ipaddress]$network_mask
+
+    }
+    #Assign two IP addresses to 2 variables 
+    $ip1 = [ipaddress]$ip_address_1
+    $ip2 = [ipaddress]$ip_address_2
+
+    #Check if they belong in the same network using -band for binary comparison of two IP addresses
+    if (($ip1.address -band $mask.address) -eq ($ip2.address -band $mask.address)) {
+        Write-Output "Yes"
+    }
+    else {
+        Write-Output "No"
+    }
+
+}
---
+++ task2_bash/task2.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#Using gsub() to add commas to make text file suitable to convert and make temproary
+awk -F' ' '{gsub(/expecting/, ",\"expecting"); gsub(/ok/, "ok,"); gsub(/, /, ","); gsub("\\)", ")\""); gsub(/,bats/, ", bats"); gsub("\\[ ", "["); gsub(" \\]", "]"); print}' ./$1  > ./tmp.txt 
+
+#Make variable which store name of test. Name of test inside of the sqare brackets so I use -F'[][]'
+name=`awk -F'[][]' '{print $2}' ./tmp.txt`
+
+#Print first curly brackets then print the name of the script inside of json file
+#UPDATE! Name was without double quotes then I add "\" ... \" and it solve that issue 
+echo { > ./output.json 
+
+awk -v n="$name" BEGIN'{print "\"testName\":\"" n "\"," }' ./tmp.txt  >> ./output.json
+
+#Summ of tests 
+num_of_tests=`awk -F"[ , ][ ,]*" 'END{print $1 + $6}' ./tmp.txt`
+
+#Now converting body of text and save it inside of .json file and additional "if-else" statement to find last test using id($2) then remove comma after curly bracket "}"
+awk -v x="$num_of_tests" -vFPAT='([^,]*)|("[^"]+")'  'BEGIN{print  "\"tests\":""""[" ; }   
+
+	{ if ($1 == "not ok")
+		{	
+		 if ($2 == x)
+			{
+				print  " {""\n"                                  \
+                   		"   \"name\":"$3",\n"                            \
+                   		"   \"status\":false,\n"            \
+                   		"   \"duration\":\""  $NF  "\"\n"                \
+                   		" }"                                             \
+
+			}
+
+		   else {
+	        		print  " {""\n"		                         \
+                   		"   \"name\":"$3",\n"                            \
+                   		"   \"status\":false,\n"            \
+                   		"   \"duration\":\""  $NF  "\"\n"                \
+                   		" },"                                            \
+			}                                          
+                }   			
+	
+	  else if ($1 == "ok")
+		{
+			
+		   if ($2 == x)
+                         {
+                                print  " {""\n"                                  \
+                  		"   \"name\":"$3",\n"                            \
+                  		"   \"status\":true,\n"             \
+                  		"   \"duration\":\""  $NF  "\"\n"                \
+                  		" }"                                             \
+
+                         }
+		    else {
+
+				print  " {""\n"                                  \
+                  		"   \"name\":"$3",\n"                            \
+                  		"   \"status\":true,\n"             \
+                  		"   \"duration\":\""  $NF  "\"\n"                \
+                  		" },"				         	 \
+
+		         }
+							     	
+                }				              
+
+}
+
+END{print "],"}' OFS=,  ./tmp.txt >> ./output.json
+
+#Converting end of file and store in .json file and "CONVFMT="%.2f"" to round up 2 decimal point
+awk  -F"[ , ][ ,]*" -v CONVFMT="%.2f" 'END{printf  "\"summary\"" ":""{""\n"	                         \
+           "   \"success\":"$1",\n"                                        \
+           "   \"failed\":"$6",\n"                                       \
+	   "   \"rating\":"$1 / ($1 + $6) * 100",\n"                                      \
+	   "   \"duration\":\""  $NF  "\"\n"                                     \
+	   " }"							                 \
+		
+   }' ./tmp.txt  >> ./output.json
+echo }  >> ./output.json
+
+#Remove tmp file
+rm ./tmp.txt
